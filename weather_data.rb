048e6a841f5aed191f1a4d56c1b4f1031155a257
Hector Hernandez <hhernandez@appen.com>
2024-06-22 10:49:53 -0500

    Add method to read file and parse it into a hash

--- weather_data.rb
+++ weather_data.rb
@@ -1,3 +1,17 @@
 class WeatherData
+  attr_accessor :data
 
+  def initialize(file_path)
+    file = File.read(file_path)
+    @data = get_data(file)
+  end
+  private
+
+  def get_data(file)
+    table_lines = file.split("\n").select { |line| line.length == 89 }
+    columns = table_lines.first.strip.split
+    table_lines[1..-1].map do |line|
+      columns.zip(line.strip.split).to_h
+    end
+  end
 end
