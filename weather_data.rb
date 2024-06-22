4032d7ec7f5db87dab105fb6c1e38747adec75fe
Hector Hernandez <hhernandez@appen.com>
2024-06-22 10:50:43 -0500

    Add method to calculate the minimum spread

--- weather_data.rb
+++ weather_data.rb
@@ -5,6 +5,20 @@ class WeatherData
     file = File.read(file_path)
     @data = get_data(file)
   end
+
+  def calculate_day_with_minimun_spread
+    min_spread = nil
+    min_spread_day = nil
+    @data.each do |day|
+      spread = get_spread(day['MnT'], day['MxT'])
+      if min_spread.nil? || spread < min_spread
+        min_spread = spread
+        min_spread_day = day['Dy']
+      end
+    end
+    min_spread_day
+  end
+
   private
 
   def get_data(file)
@@ -13,5 +27,9 @@ class WeatherData
     table_lines[1..-1].map do |line|
       columns.zip(line.strip.split).to_h
     end
+  end
+
+  def get_spread(min, max)
+    max.to_f - min.to_f
   end
 end
